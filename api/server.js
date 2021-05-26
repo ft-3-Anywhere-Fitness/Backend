a005055e5049f9bc5687d7083ebc6c32cd5c9502
unknown <agreer.ag12@gmail.com>
2021-05-26 14:59:24 -0400

    fitness router is now up and running

--- api/server.js
+++ api/server.js
@@ -1,12 +1,17 @@
 const express = require('express')
 const helmet = require('helmet')
 const cors = require('cors')
+const fitnessRouter = require('./fitnessRouter/router')
 
 const server = express()
 server.use(express.json())
 server.use(helmet())
 server.use(cors())
 
+// API ENDPOINT
+server.use('/api/classes', fitnessRouter)
+
+// sanity check
 server.get('/', (req, res) => {
     res.json({message: 'server is working!'})
 })
