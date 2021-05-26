200dce97cf1b6b059d745a523a702469fd17fe4b
unknown <agreer.ag12@gmail.com>
2021-05-26 11:52:09 -0400

    now have all needed tables

--- api/data/migrations/20210124181032_first-migration.js
+++ api/data/migrations/20210124181032_first-migration.js
@@ -2,15 +2,29 @@ exports.up = async (knex) => {
   await knex.schema
     .createTable('users', (users) => {
       users.increments('user_id')
+      users.timestamps('created_at')
       users.string('user_username', 200).notNullable()
       users.string('user_password', 200).notNullable()
       users.string('user_email', 320).notNullable()
-      users.timestamps(false, true)
       users.string('user_auth', 200)
       users.integer('user_type').defaultTo(1)
+    })
+
+    .createTable('fitness_classes', (classes) => {
+      classes.increments('fitness_class_id')
+      classes.string('fitness_class_name', 320).notNullable()
+      classes.string('fitness_class_img').defaultTo('https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/fitness-hangover-1565770628.jpg')
+      classes.string('fitness_class_type', 320).notNullable()
+      classes.string('start_time', 320).notNullable()
+      classes.string('duration', 320).notNullable()
+      classes.string('intensity_level', 320).notNullable()
+      classes.string('location', 320).notNullable()
+      classes.integer('fitness_class_attendees', 200).notNullable()
+      classes.integer('fitness_class_max').notNullable()
     })
 }
 
 exports.down = async (knex) => {
   await knex.schema.dropTableIfExists('users')
+  await knex.schema.dropTableIfExists('fitness_classes')
 }
