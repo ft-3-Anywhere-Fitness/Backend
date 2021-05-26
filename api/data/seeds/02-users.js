b9872ef0cd5c860f6b91ab23fc711f6b9ecf5a8e
unknown <agreer.ag12@gmail.com>
2021-05-26 13:31:43 -0400

    working on dummy data

---
+++ api/data/seeds/02-users.js
@@ -0,0 +1,9 @@
+
+exports.seed = function(knex) {
+      return knex('users').insert([
+        {user_username: 'austin', user_password: "abc123", user_type: 1},
+        {user_username: 'Kelly', user_password: "abc123", user_type: 1},
+        {user_username: 'Jenna', user_password: "abc123", user_type: 1},
+        { user_username: 'Humza', user_password: "helloworld", user_type: 1}
+      ]);
+};
